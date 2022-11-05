9f903d1bc57ab5c205e27861737c078f81b11082
Yingzhiemily <emilyyingzhi@gmail.com>
2022-11-05 21:16:19 +0200

    submit

---
+++ website/src/components/ContactUs.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+export default function ContactUs() {
+    return (
+        <>
+        <div><strong>Email:</strong>&nbsp;t1zhyi00@students.oamk.fi</div>
+        </>
+    )
+    
+}
